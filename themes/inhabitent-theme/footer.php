1d76f6709883f1455ca5af1cc6bfab888699f85c
Djordje Vujatovic <djordje_v@hotmail.com>
2016-11-04 16:16:49 -0700

    first commit

---
+++ themes/inhabitent-theme/footer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				 <div class= "company-info">
+				   <div class= "contact-info">
+					<h4>Contact Info</h4>
+					<a href = "main to: djordje_v@hotmail.com">djordje_v@hotmail.com</a>
+					<p>778-688-7121</p>
+				   </div>
+				   <div class= "business-hours">
+					<h4>Business Hours</h4>
+					<p>Monday-Friday: 9am - 5pm</p>
+					<p>Saturday: 10am - 2pm</p>
+					<p>Sunday: Closed</p>
+				   </div>
+				   <div class = "footer-logo">
+				    <img src="<?php echo get_template_directory_uri(); ?>/images/inhabitent-logo-text.svg">  
+				   </div>
+				   <div class= "copyright-tag">
+				   	<p>Copyright © 2016 Inhabitent</p>
+				   </div>
+				 </div>
+
+				 <div class="site-info">
+					<!--<a href="<?php //echo esc_url( 'https://wordpress.org/' ); ?>"><?php //printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>-->
+
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
