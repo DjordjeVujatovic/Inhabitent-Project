ff10c6bbc6a831954b8ca08aaa628ad8685f4c16
Djordje Vujatovic <djordje_v@hotmail.com>
2016-11-09 13:36:05 -0800

    Styled side bar, added color to archive, took off list style and text decoration.

--- themes/inhabitent-theme/sidebar.php
+++ themes/inhabitent-theme/sidebar.php
@@ -32,17 +32,9 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-<section class = "side-bar>">
-<div class= "contact-info-sidebar">
-    <h3>Contact Info</h3>
-    <p><a href="tel:778-456-7891">
-	<i class="fa fa-phone" aria-hidden="true"></i> 778-456-7891</a></p>
-    <p><i class="fa fa-envelope-o" aria-hidden="true"></i><a href="info@inhabitent.com"> info@inhabitent.com</a></p>
-    <p><i class="fa fa-map-marker" aria-hidden="true"></i> 1490 W Broadway <br>Vancouver, BC V6H 1H5</p>
-</div>
+
 
 <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 
 </div><!-- #secondary -->
-</section>
