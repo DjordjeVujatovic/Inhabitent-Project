1cbcb66f1856eb572802d1fcf5f9b51e7813bc01
Djordje Vujatovic <djordje_v@hotmail.com>
2016-11-13 08:44:36 -0800

    Added font and uppercase to copy right section of footer, added telephone font awesome to phone number in footer.

--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -7,41 +7,42 @@
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 <!--Site Footer-->
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				 <div class= "company-info">
-				   <div class= "contact-info">
-					<h4>Contact Info</h4>
-						<i class="fa fa-envelope" aria-hidden="true"></i>
-					<a href = "main to: djordje_v@hotmail.com">djordje_v@hotmail.com</a>					
-					<p>778-688-7121</p>
-						<i class="fa fa-facebook-square" aria-hidden="true"></i>
-						<i class="fa fa-twitter-square" aria-hidden="true"></i>
-						<i class="fa fa-google-plus-square" aria-hidden="true"></i>
-				   </div>
-				   <div class= "business-hours">
-				   	<h4>Business Hours</h4>
-				   	<p class = "day-of-week">Monday-Friday: <span class = "hours">9am - 5pm</span></p>
-				   	<p class = "day-of-week">Saturday: <span class = "hours">10am - 2pm</span></p>
-				   	<p class = "day-of-week">Sunday: <span class = "hours">Closed</span></p>
-				   </div>
-				   <div class = "footer-logo">
-				    <img src="<?php echo get_template_directory_uri(); ?>/images/inhabitent-logo-text.svg">  
-				   </div>
-				   <div class= "copyright-tag">
-				   	<p>Copyright © 2016 Inhabitent</p>
-				   </div>
-				 </div>
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class= "company-info">
+		<div class= "contact-info">
+			<h4>Contact Info</h4>
+			<i class="fa fa-envelope" aria-hidden="true"></i>
+			<a href = "main to: djordje_v@hotmail.com"> djordje_v@hotmail.com</a>
 
-				 <div class="site-info">
-					<!--<a href="<?php //echo esc_url( 'https://wordpress.org/' ); ?>"><?php //printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>-->
+			<p><i class="fa fa-phone" aria-hidden="true"></i> 778-688-7121</p>
+			<i class="fa fa-facebook-square" aria-hidden="true"></i>
+			<i class="fa fa-twitter-square" aria-hidden="true"></i>
+			<i class="fa fa-google-plus-square" aria-hidden="true"></i>
+		</div>
+		<div class= "business-hours">
+			<h4>Business Hours</h4>
+			<p class = "day-of-week">Monday-Friday: <span class = "hours">9am - 5pm</span></p>
+			<p class = "day-of-week">Saturday: <span class = "hours">10am - 2pm</span></p>
+			<p class = "day-of-week">Sunday: <span class = "hours">Closed</span></p>
+		</div>
+		<div class = "footer-logo">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/inhabitent-logo-text.svg">  
+		</div>
+		<div class= "copyright-tag">
+			<p>Copyright © 2016 Inhabitent</p>
+		</div>
+	</div>
 
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+	<div class="site-info">
+		<!--<a href="<?php //echo esc_url( 'https://wordpress.org/' ); ?>"><?php //printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>-->
 
-		<?php wp_footer(); ?>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
 
-	</body>
+<?php wp_footer(); ?>
+
+</body>
 </html>
