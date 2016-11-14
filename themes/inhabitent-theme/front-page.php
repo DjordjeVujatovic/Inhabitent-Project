79ecb3f691eaaee2951d19b19d8d21e8ef3b15ed
Djordje Vujatovic <djordje_v@hotmail.com>
2016-11-13 18:16:20 -0800

    Added headings, styling, color, fonts to adventure section.

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -7,40 +7,40 @@
 
 get_header(); ?>
 <body class = "front-page-container">
-<div class="home-hero-image"></div>
-<section class = "product-feed-container">
-  <h2 class = "front-page-heading">shop stuff</h2>
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-    
-
-      <?php while ( have_posts() ) : the_post(); ?>
-
-       <?php get_template_part( 'template-parts/content', 'page' ); ?>
+  <div class="home-hero-image"></div>
+  <section class = "product-feed-container">
+    <h2 class = "front-page-heading">shop stuff</h2>
+    <div id="primary" class="content-area">
+      <main id="main" class="site-main" role="main">
 
 
-     <?php endwhile; // End of the loop. ?>
+        <?php while ( have_posts() ) : the_post(); ?>
 
-     <?php
-     $terms = get_terms('product_type');
-     foreach ($terms as $term) :
-      ?>
-
-    <?php $url = get_term_link($term->slug, 'product_type'); ?>
-    <div class = "individual-sections">
-      <img src="<?php echo get_template_directory_uri() ?>/images/<?php echo $term->slug?>.svg">
-      <p><?php echo $term->description ?></p>
-      <p><a href="<?php echo $url ?>"><?php echo $term->name ?></a></p>
-    </div>
-  <?php endforeach;?>
+         <?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 
+       <?php endwhile; // End of the loop. ?>
 
-</main><!-- #main -->
+       <?php
+       $terms = get_terms('product_type');
+       foreach ($terms as $term) :
+        ?>
+
+      <?php $url = get_term_link($term->slug, 'product_type'); ?>
+      <div class = "individual-sections">
+        <img src="<?php echo get_template_directory_uri() ?>/images/<?php echo $term->slug?>.svg">
+        <p><?php echo $term->description ?></p>
+        <p><a href="<?php echo $url ?>"><?php echo $term->name ?></a></p>
+      </div>
+    <?php endforeach;?>
+
+
+
+  </main><!-- #main -->
 </div><!-- #primary -->
 </section>
 <secttion class = "inhabitent-journal-title">
-<h2 class = "front-page-heading">Inhabitent Journal</h2>
+  <h2 class = "front-page-heading">Inhabitent Journal</h2>
 </secttion>
 <div class = "inhabitent-journal">
   <?php
@@ -54,37 +54,47 @@ get_header(); ?>
     <div class ="individual-journal-post">
      <?php the_post_thumbnail(); ?>
      <div class = "journal-post-container">
-     <div class ="date-comment">
-     <?php the_date();?>
-     <?php comments_number();?>
+       <div class ="date-comment">
+         <?php the_date();?>
+         <?php comments_number();?>
+       </div>
+       <h3><?php the_title();?></h3>
+       <a class ="read" href="<?php the_permalink();?>">Read Entry</a>
      </div>
-     <h3><?php the_title();?></h3>
-     <a class ="read" href="<?php the_permalink();?>">Read Entry</a>
-    </div>
-    </div>
-   <?php endforeach; wp_reset_postdata(); ?>
- </div>
- <section class="adventures-container">
-   <h2 class = "front-page-heading">Latest Adventures</h2>
-   <div class="adventure-section">
-    <div class="left-box">
-  	<!--<h3 class = "entry-title"><a href= "">Getting Back to Nature in a Canoe</a></h3>
-  	<a class = "read-more-button" href = "">Read More</a>-->
+   </div>
+ <?php endforeach; wp_reset_postdata(); ?>
+</div>
+<section class="adventures-container">
+ <h2 class = "front-page-heading">Latest Adventures</h2>
+ <div class="adventure-section">
+  <div class="left-box">
+   <div class ="story-info">
+  	<h3 class = "entry-title-left"><a href= "">Getting Back to Nature in a Canoe</a></h3>
+  	<a class = "read-more-article" href = "">Read More</a>
+   </div>
   </div>
 
   <div class="right-mix">
     <div class="top-right">
       <div class="right-box-top">
+      <div class= "story-info">
+      <h3 class = "entry-title-top-right"><a href= "">A Night with Friends at the Beach</a></h3>
+        <a class = "read-more-article" href = "">Read More</a>
+       </div>
       </div>
     </div>
     <div class="right-box-bottom">
       <div class="bottom-right">
-      	<!--<h3 class = "entry-title"><a href= "">Taking in the View at Big Mountain</a></h3>
-      	<a class = "read-more-button" href = "">Read More</a>-->
+      <div class ="story-info">
+      	<h3 class = "entry-title-bottom-left"><a href= "">Taking in the View at Big Mountain</a></h3>
+      	<a class = "read-more-article" href = "">Read More</a>
+      </div>
       </div>
       <div class="bottom-left">
-      	<!--<h3 class = "entry-title"><a href= "">Star-Gazing at the Night Sky</a></h3>
-      	<a class = "read-more-button" href = "">Read More</a>-->
+      <div class ="story-info">
+      	<h3 class = "entry-title-bottom-right"><a href= "">Star-Gazing at the Night Sky</a></h3>
+      	<a class = "read-more-article" href = "">Read More</a>
+      </div>
       </div>
     </div>
   </div>
