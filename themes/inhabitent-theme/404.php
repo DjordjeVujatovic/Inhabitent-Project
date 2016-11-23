527c4039367fe8e3a9df53049f15f67edb3b6528
Djordje Vujatovic <djordje_v@hotmail.com>
2016-11-23 13:02:36 -0800

    Fixed search field on 404 page.

--- themes/inhabitent-theme/404.php
+++ themes/inhabitent-theme/404.php
@@ -19,7 +19,7 @@ get_header(); ?>
 
 				<div class="page-content">
 					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
-
+				  
 					<div class="search-container"><?php get_search_form(); ?></div>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
