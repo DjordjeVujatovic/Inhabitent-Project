55a09991478225bf6d51f618512b498a4eeabbb1
Djordje Vujatovic <djordje_v@hotmail.com>
2017-03-10 15:00:25 -0800

    Cleaned up template-tags code.

--- themes/inhabitent-theme/inc/template-tags.php
+++ themes/inhabitent-theme/inc/template-tags.php
@@ -8,53 +8,50 @@
  /**
   * Prints HTML with meta information for the current post-date/time.
   */
+
  function red_starter_posted_on() {
  	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
  	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
- 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+ 	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
  	}
-
  	$time_string = sprintf( $time_string,
- 		esc_attr( get_the_date( 'c' ) ),
- 		esc_html( get_the_date() ),
- 		esc_attr( get_the_modified_date( 'c' ) ),
- 		esc_html( get_the_modified_date() )
+ 	 esc_attr( get_the_date( 'c' ) ),
+ 	 esc_html( get_the_date() ),
+ 	 esc_attr( get_the_modified_date( 'c' ) ),
+ 	 esc_html( get_the_modified_date() )
  	);
-
  	$posted_on = sprintf( esc_html( '%s' ), $time_string );
-
  	echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
-
  }
 
  /**
   * Prints HTML with meta information for the post author.
   */
+
  function red_starter_posted_by() {
  	$byline = sprintf(
- 		esc_html( 'by %s' ),
- 		'<span class="author vcard">' . esc_html( get_the_author() ) . '</span>'
- 	);
-
+ 	esc_html( 'by %s' ),
+ 	'<span class="author vcard">' . esc_html( get_the_author() ) . '</span>'
+  );
  	echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
-
  }
 
  /**
   * Prints HTML with meta information for the comments with pop-up link.
   */
+
  function red_starter_comment_count() {
  	if ( is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
- 		echo '<span class="comments-link">';
- 		comments_popup_link( esc_html( '0 Comments' ), esc_html( '1 Comment' ), esc_html( '% Comments' ) );
- 		echo '</span>';
+ 	 echo '<span class="comments-link">';
+ 	 comments_popup_link( esc_html( '0 Comments' ), esc_html( '1 Comment' ), esc_html( '% Comments' ) );
+ 	 echo '</span>';
  	}
-
  }
 
  /**
   * Prints HTML with meta information for the categories and tags.
   */
+
  function red_starter_entry_footer() {
  	// Hide category and tag text for pages.
  	if ( 'post' === get_post_type() ) {
@@ -63,7 +60,6 @@
  		if ( $categories_list && red_starter_categorized_blog() ) {
  			printf( '<span class="cat-links">' . esc_html( 'Posted in &rarr; %1$s' ) . '</span>', $categories_list ); // WPCS: XSS OK.
  		}
-
  		/* translators: used between list items, there is a space after the comma */
  		$tags_list = get_the_tag_list( '', esc_html( ', ' ) );
  		if ( $tags_list ) {
@@ -83,17 +79,14 @@
  		$all_the_cool_cats = get_categories( array(
  			'fields'     => 'ids',
  			'hide_empty' => 1,
-
  			// We only need to know if there is more than one category.
  			'number'     => 2,
  		) );
 
  		// Count the number of categories that are attached to the posts.
  		$all_the_cool_cats = count( $all_the_cool_cats );
-
  		set_transient( 'red_starter_categories', $all_the_cool_cats );
  	}
-
  	if ( $all_the_cool_cats > 1 ) {
  		// This blog has more than 1 category so red_starter_categorized_blog should return true.
  		return true;
@@ -106,6 +99,7 @@
  /**
   * Flush out the transients used in red_starter_categorized_blog.
   */
+
  function red_starter_category_transient_flusher() {
  	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
  		return;
@@ -179,12 +173,12 @@
  		echo '<nav role="navigation" class="search-pagination">';
  		echo paginate_links(
  			array(
- 				'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
- 				'format' => '?paged=%#%',
- 				'current' => max( 1, get_query_var('paged') ),
- 				'total' => $wp_query->max_num_pages,
- 				'prev_text' => esc_html( '&larr; Previous' ),
- 				'next_text' => esc_html( 'Next &rarr;' ),
+ 			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+ 			'format' => '?paged=%#%',
+ 			'current' => max( 1, get_query_var('paged') ),
+ 			'total' => $wp_query->max_num_pages,
+ 			'prev_text' => esc_html( '&larr; Previous' ),
+ 			'next_text' => esc_html( 'Next &rarr;' ),
  			)
  		);
  		echo '</nav>';
