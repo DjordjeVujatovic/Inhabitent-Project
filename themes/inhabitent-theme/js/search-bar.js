7e5808c4246feba19681c057025224da02a5a39c
Djordje Vujatovic <djordje_v@hotmail.com>
2017-03-10 14:57:10 -0800

    Deleted random index.html file, cleaned up search-bar js code.

--- themes/inhabitent-theme/js/search-bar.js
+++ themes/inhabitent-theme/js/search-bar.js
@@ -1,12 +1,8 @@
-( function( $ ) {
-
-	$('.search-submit').on('click', function() {
-
-		
-		$('.search-field').animate({width: 'toggle'}).focus();	
-	})
-
-	$('.search-field').focusout(function() {
-		$('.search-field').animate({width: 'toggle' });
-	}) 
-})( jQuery );
+(function ($) {
+ $('.search-submit').on('click', function () {
+  $('.search-field').animate({ width: 'toggle' }).focus();
+ })
+ $('.search-field').focusout(function () {
+  $('.search-field').animate({ width: 'toggle' });
+ })
+})(jQuery);
