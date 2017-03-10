e07db0197b05013cb3d4468560e6a409e2e63373
Djordje Vujatovic <djordje_v@hotmail.com>
2017-03-10 15:01:58 -0800

    Cleaned up about page php code.

--- themes/inhabitent-theme/about.php
+++ themes/inhabitent-theme/about.php
@@ -12,17 +12,16 @@
 ?>
 <?php get_header(); ?>
 <div id="primary-about" class="content-area-about">
-	<main id="main" class="site-main" role="main">
-		<section class="about-hero">
-			<h2><?php single_post_title() ?></h2>
-			</section>
-			<div class = "about-container">
-			  <div class = "about-section">
-				<?php echo CFS()->get( 'our_story' ); ?>
-				<?php echo CFS()->get( 'our_team' ); ?>
- 			 </div>
-			</div>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<?php get_footer(); ?>
+ <main id="main" class="site-main" role="main">
+  <section class="about-hero">
+	<h2><?php single_post_title() ?></h2>
+	 </section>
+	  <div class = "about-container">
+		<div class = "about-section">
+		  <?php echo CFS()->get( 'our_story' ); ?>
+		  <?php echo CFS()->get( 'our_team' ); ?>
+ 		</div>
+	  </div>
+  </main><!-- #main -->
+</div><!-- #primary -->
+<?php get_footer(); ?>
